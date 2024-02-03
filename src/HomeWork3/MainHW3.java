84e5170c8cbcda691eada32fc30703c37cd70414
FedorSherbinin <fredersherb@gmail.com>
2024-02-04 01:24:11 +0300

    HomeWork3

---
+++ src/HomeWork3/MainHW3.java
@@ -0,0 +1,37 @@
+package HomeWork3;
+
+import HomeWork3.apothecary.Pharmacy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainHW3 {
+    public static void main(String[] args) {
+        ArrayList<Pharmacy> pharmacies = new ArrayList<>();
+
+        // Создаем экземпляры Pharmacy и добавляем их в список
+        pharmacies.add(new Pharmacy("BioShield Max", 50));
+        pharmacies.add(new Pharmacy("HealixUltra", 30));
+        pharmacies.add(new Pharmacy("EnergyRestore", 40));
+        pharmacies.add(new Pharmacy("VitalityBlend", 45));
+        pharmacies.add(new Pharmacy("RevitalizeX", 35));
+
+        // Сортируем по цене, используя Comparable
+        Collections.sort(pharmacies);
+
+        // Выводим отсортированный список
+        System.out.println("Sorted by price:");
+        for (Pharmacy pharmacy : pharmacies) {
+            System.out.println(pharmacy.getName() + " - " + pharmacy.getPrice());
+        }
+
+        // Сортируем по имени, используя дополнительный метод
+        Collections.sort(pharmacies, Pharmacy::compareByName);
+
+        // Выводим отсортированный список по имени
+        System.out.println("\nSorted by name:");
+        for (Pharmacy pharmacy : pharmacies) {
+            System.out.println(pharmacy.getName() + " - " + pharmacy.getPrice());
+        }
+    }
+}
---
+++ src/HomeWork3/apothecary/Pharmacy.java
@@ -0,0 +1,28 @@
+package HomeWork3.apothecary;
+
+public class Pharmacy implements Comparable<Pharmacy>{
+    private String name;
+    private int price;
+
+    public Pharmacy(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    @Override
+    public int compareTo(Pharmacy o) {
+        return Integer.compare(this.price, o.price);
+    }
+
+    public static int compareByName(Pharmacy p1, Pharmacy p2){
+        return p1.name.compareTo(p2.name);
+    }
+
+}
